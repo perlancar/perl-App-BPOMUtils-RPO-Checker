55bc0e259eb236d0602110a6b8ba47f7ddce2562
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-02-11 11:02:00 +0700

    [incompatible change] Rename bpom-repo-check-{,files-}label-design; add utility bpom-rpo-check-files; add check: multiple dots, symbols in filename

--- lib/App/BPOMUtils/RPO/Checker.pm
+++ lib/App/BPOMUtils/RPO/Checker.pm
@@ -13,7 +13,7 @@ use Exporter 'import';
 # VERSION
 
 our @EXPORT_OK = qw(
-                       bpom_rpo_check_label_design
+                       bpom_rpo_check_label_files_design
                );
 
 our %SPEC;
@@ -23,7 +23,63 @@ $SPEC{':package'} = {
     summary => 'Various checker utilities to help with Processed Food Registration (RPO - Registrasi Pangan Olahan) at BPOM',
 };
 
-$SPEC{bpom_rpo_check_label_design} = {
+$SPEC{bpom_rpo_check_files} = {
+    v => 1.1,
+    summary => 'Check document files',
+    description => <<'_',
+
+By default will check all files in the current directory, recursively.
+
+Here's what it checks:
+- filename should not contain unsafe symbols
+- file must not be larger than 5MB
+- file must be readable
+
+_
+    args => {
+        files => {
+            schema => ['array*', of=>'filename', 'x.perl.default_value_rules' => [['Path::filenames']]],
+            pos => 0,
+            slurpy => 1,
+        },
+    },
+};
+sub bpom_rpo_check_files {
+    my %args = @_;
+
+    my $i = 0;
+    my @errors;
+    my @warnings;
+    for my $file (@{ $args{files} }) {
+        $i++;
+        log_info "[%d/%d] Processing file %s ...", $i, scalar(@{ $args{files} }), $file;
+        unless (-f $file) {
+            push @errors, {file=>$file, message=>"File not found or not a regular file"};
+            next;
+        }
+
+        if ($file =~ /\.[^.]+\./) {
+            push @errors, {file=>$file, message=>"Filename contains multiple dots, currently uploadable but not viewable in ereg-rba"};
+        }
+        if ($file =~ /[^A-Za-z0-9_.-]/) {
+            push @warnings, {file=>$file, message=>"Filename contains symbols, should be avoided to ensure viewable in ereg-rba"};
+        }
+
+        if (!-r($file)) {
+            push @errors, {file=>$file, message=>"File cannot be read"};
+            next;
+        }
+
+        my $filesize = -s $file;
+        if ($filesize > 5*1024*1024) {
+            push @errors, {file=>$file, message=>"File size too large (>5M)"};
+        }
+    }
+
+    [200, "OK", [@errors, @warnings], {'cmdline.exit_code'=>@errors ? 1:0}];
+}
+
+$SPEC{bpom_rpo_check_files_label_design} = {
     v => 1.1,
     summary => 'Check label design files',
     description => <<'_',
@@ -32,7 +88,9 @@ By default will check all files in the current directory, recursively.
 
 Here's what it checks:
 - file must be in JPEG format and has name ending in /\.jpe?g$/i
+- filename should not contain unsafe symbols
 - file must not be larger than 5MB
+- file must be readable
 - image size must not be larger than 2300 x 2300 px
 - (WARNING) image should not be smaller than 600 x 600 px
 
@@ -45,7 +103,7 @@ _
         },
     },
 };
-sub bpom_rpo_check_label_design {
+sub bpom_rpo_check_files_label_design {
     require File::MimeInfo::Magic;
     require Image::Size;
 
@@ -61,9 +119,17 @@ sub bpom_rpo_check_label_design {
             push @errors, {file=>$file, message=>"File not found or not a regular file"};
             next;
         }
+
         unless ($file =~ /\.jpe?g\z/i) {
             push @errors, {file=>$file, message=>"Filename does not end in .JPG or .JPEG"};
         }
+        if ($file =~ /\.[^.]+\./) {
+            push @errors, {file=>$file, message=>"Filename contains multiple dots, currently uploadable but not viewable in ereg-rba"};
+        }
+        if ($file =~ /[^A-Za-z0-9_.-]/) {
+            push @warnings, {file=>$file, message=>"Filename contains symbols, should be avoided to ensure viewable in ereg-rba"};
+        }
+
         if (!-r($file)) {
             push @errors, {file=>$file, message=>"File cannot be read"};
             next;
